8e1b028ed1b52ebdad89b8ce0a10f272a613239c
fawzy20 <fawzy-elsam@outlook.fr>
2024-06-12 23:47:31 +0200

    modification des route de l'api gateway

--- api-gateway/src/main/java/com/example/api_gateway/configuraton/GatewayDiscoveryConfiguration.java
+++ api-gateway/src/main/java/com/example/api_gateway/configuraton/GatewayDiscoveryConfiguration.java
@@ -1,6 +1,11 @@
 package com.example.api_gateway.configuraton;
 
+import com.netflix.discovery.DiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,26 +13,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@EnableDiscoveryClient
 public class GatewayDiscoveryConfiguration {
-
-    @LoadBalanced
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    DiscoveryClientRouteDefinitionLocator discoveryRoutes(ReactiveDiscoveryClient dc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(dc, dlp);
     }
 
-    @Bean
-    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-        return builder.routes()
-                .route("gestionMessage",p -> p
-                        .path("/gestionMessage/**")
-                        .uri("lb://GESTIONMESSAGE"))
-                .route("gestionProfil",p -> p
-                        .path("/gestionProfil/**")
-                        .uri("lb://GESTIONPROFIL"))
-                .route("gestionUtilisateur",p -> p
-                        .path("/gestionUtilisateur/**")
-                        .uri("lb://GESTIONUTILISATEUR"))
-                .build();
-    }
 }
