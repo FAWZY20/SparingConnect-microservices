5465339d0a7ce1d4e00a0d2328d7b8ba43f3d3df
fawzy20 <fawzy-elsam@outlook.fr>
2024-06-27 15:02:21 +0200

    modification api-gateway security

--- api-gateway/src/main/java/com/example/api_gateway/configuraton/GatewayDiscoveryConfiguration.java
+++ api-gateway/src/main/java/com/example/api_gateway/configuraton/GatewayDiscoveryConfiguration.java
@@ -1,16 +1,11 @@
 package com.example.api_gateway.configuraton;
 
-import com.netflix.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
 @Configuration
 @EnableDiscoveryClient
